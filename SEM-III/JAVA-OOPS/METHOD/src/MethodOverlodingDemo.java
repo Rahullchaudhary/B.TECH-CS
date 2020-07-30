8cc58b193995094c069fab0af513c7241c6862b0
Priyanshu Vishwakarma <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-07-30 15:43:26 +0530

    OS ASSIGNMENT 2 COMPLETED

---
+++ SEM-III/JAVA-OOPS/METHOD/src/MethodOverlodingDemo.java
@@ -0,0 +1,16 @@
+/*
+Create a method add,which accept two integer arguments
+and create another method
+*/
+
+
+
+public class MethodOverlodingDemo {
+    public static void main(String[] args) {
+
+    }
+    public int add(int no1, int no2){
+        int response = 0;
+        response = no1 + no2;
+    }
+}
