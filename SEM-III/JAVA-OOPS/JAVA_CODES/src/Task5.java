8d98a6bcd0791d38e231777cddc8b15e0f8b819b
PriyanshuV <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-04 19:00:43 +0530

    TASK 5

---
+++ SEM-III/JAVA-OOPS/JAVA_CODES/src/Task5.java
@@ -0,0 +1,33 @@
+/*
+Print any given inputString in following format.
+Output
+index:  0    1   2   3   4
+chars:  H    e   l   l   o
+Every character is separated by tab, use "\t" for inserting tab.
+*/
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String : ");
+        String inputString = sc.nextLine();
+        System.out.print("index -: ");
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(i);
+            if (i < inputString.length() -1){
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+        System.out.print("chars -: ");
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(inputString.charAt(i));
+            if (i < inputString.length() -1){
+                System.out.print("\t");
+            }
+        }
+    }
+
+}
