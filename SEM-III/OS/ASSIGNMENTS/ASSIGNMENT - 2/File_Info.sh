256b1063e2e2d35cba06efa81aee3d9be1186825
CodePredator01 <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-08 00:07:14 +0530

    OS ASSIGNMENT COMPLETED !

--- SEM-III/OS/ASSIGNMENTS/ASSIGNMENT - 2/File_Info.sh
+++
@@ -1,4 +0,0 @@
-# !/usr/bin/bash
-
-echo "Process Name Of Current File : $0"
-echo "Process Id Of Current File : $$"
---
+++ SEM-III/OS/ASSIGNMENTS/ASSIGNMENT - 5/File_Info.sh
@@ -0,0 +1,4 @@
+# !/usr/bin/bash
+
+echo "Process Name Of Current File : $0"
+echo "Process Id Of Current File : $$"
